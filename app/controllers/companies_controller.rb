e6cce7f488ce90dd500045e89a29b46323b1a01e
kaitdoch Docherty <kaitlyn.docherty@kellogg.northwestern.edu>
2025-05-13 16:34:42 +0000

    in class

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,25 +1,35 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all 
     # find all Company rows
     # render companies/index view
   end
 
   def show
     # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    @contacts = Contact.where({"company_id" => @company["id"]})
     # render companies/show view with details about Company
   end
 
   def new
+    @company = Company.find_by({"id" => params["company_id"]})
     # render view with new Company form
   end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Company
+    @company = Company.new
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+    # save Company row
+    @company.save
+    # redirect user
+    redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
